ff17b2ab792f00eb0f2bf4711898e6addcab67ae
Chau Nguyen <songchau99@gmail.com>
2020-08-26 10:01:41 -0500

    Implemented remove button successfully

--- interaction.js
+++ interaction.js
@@ -3,9 +3,12 @@ const shelves = document.querySelector('.shelves');
 const newbook = document.querySelector('.newbook');
 const container = document.getElementById('container');
 const newBookForm = document.getElementById('newBookForm');
-var removeButton = document.createElement('button');
-removeButton.innerText = "Remove";
+//var removeButton = document.createElement('button');
+//removeButton.innerText = "Remove";
 
+window.onload = function(){
+    render_after_remove();
+};
 
 function Book(title, author, chapters, read)
 {
@@ -24,11 +27,14 @@ function addBookToLibrary(title, author, chapters, read)
 }
 
 
-function render()
+function render(index)
 {
     if (myLibrary.length > 0)
     {
-        var book = myLibrary[myLibrary.length - 1];
+        var book = myLibrary[index];
+        var removeButton = document.createElement('button');
+        removeButton.innerText = "Remove";
+        removeButton.id = `${index}`;
 
         let row = shelves.insertRow();
         let cell1 = row.insertCell(0);
@@ -41,25 +47,27 @@ function render()
         cell2.innerHTML = book.author;
         cell3.innerHTML = book.chapters;
         cell4.innerHTML = book.read;
-        cell5.appendChild(removeButton.cloneNode(true));
-    }
-    
-    
-    /*myLibrary.forEach(book =>
-    {
-        let row = shelves.insertRow();
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
+        cell5.appendChild(removeButton);
 
-        cell1.innerHTML = book.title;
-        cell2.innerHTML = book.author;
-        cell3.innerHTML = book.chapters;
-        cell4.innerHTML = book.read;
-    });*/
+        removeButton.onclick = function()
+        {
+            //console.log("remove button is clicked");
+            var deleted_index = row.rowIndex;
+            console.log(deleted_index);
+            myLibrary.splice(deleted_index-1,1);
+            //console.log(`number of books remained: ${myLibrary.length}`);
+            shelves.deleteRow(deleted_index);
+        };
+    }
 }
 
+function render_after_remove()
+{
+    for (let i = 0; i < myLibrary.length; i++)
+    {
+        render(i);
+    }
+}
 
 newbook.addEventListener('click', (e) =>
 {
@@ -77,9 +85,9 @@ newBookForm.addEventListener('submit', (e) =>
 
     addBookToLibrary(title, author, chapters, read);
 
-    console.log(myLibrary.length);
+    //console.log(myLibrary.length);
 
-    render();
+    render(myLibrary.length-1);
 
     console.log('reach render()');
 
@@ -92,86 +100,10 @@ newBookForm.addEventListener('submit', (e) =>
 
 
 
-//addBookToLibrary("LangBiang", "NNA", 29, true);
-//addBookToLibrary('Harry', 'Unkown', 15, false);
-render();
+addBookToLibrary("LangBiang", "NNA", 29, true);
+addBookToLibrary('Harry', 'Unkown', 15, false);
 
 
-/*newbook.addEventListener('click', (e) =>
-{
-    console.log("button is clicked");
-    newbook.disabled = true;
-    let br = document.createElement("br");
-    //pull out a form
-
-    let form = document.createElement("form");
-    form.setAttribute("method", "post");
-    //form.setAttribute("action", "submit.php");
-
-    //create input element for title
-    let title = document.createElement("input");
-    title.setAttribute("type", "text");
-    title.setAttribute("name", "title");
-    title.setAttribute("placeholder", "Title");
-    title.id = "title";
-
-   //create input element for author
-   let author = document.createElement("input");
-   author.setAttribute("type", "text");
-   author.setAttribute("name", "author");
-   author.setAttribute("placeholder", "Author");
-   author.id = "author";
-
-   //create input element for chapters
-   let chapters = document.createElement("input");
-   chapters.setAttribute("type", "number");
-   chapters.setAttribute("name", "chapters");
-   chapters.setAttribute("placeholder", "Chapters");
-   chapters.id = "chapters";
-
-   //create input element for read checkbox
-   let text = document.createTextNode("Check if you have read this book");
-   let read = document.createElement("input");
-   read.setAttribute("type", "checkbox");
-   //chapters.setAttribute("value", "Yes");
-
-    //create a submit button
-    //let s = document.createElement("button");
-    let s = document.createElement("input");
-    s.setAttribute("type", "submit");
-    s.setAttribute("value", "Submit");
-    //s.setAttribute("content", "Submit");
-
-
-    form.appendChild(title);
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(author);
-    form.appendChild(br.cloneNode());
-
-
-    form.appendChild(chapters);  //fix negative number options!!!
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(text);
-    form.appendChild(read);
-    form.appendChild(br.cloneNode());
-    form.appendChild(s);
-
-    container.appendChild(form);
-
-    //const submit = form.querySelector('button[type="submit"]');
-
-    form.addEventListener('submit', (e) =>
-    {
-        //console.log("submit is clicked");
-        let title_value = document.getElementById('title').value;
-        let author_value = document.getElementById('author').value;
-        //let chapters_value = chapters.textContent;
-
-        console.log(title_value);
-    });
-});*/
 
 
 
